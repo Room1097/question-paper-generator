eb97db2f20f4cbe81dfccdd891c74deb5e611fa4
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2024-04-13 10:24:03 +0530

    added add subject functionality connected with DB and view subject table is also connected to db

--- app/(client)/(subjects)/(routes)/subjects/page.tsx
+++ app/(client)/(subjects)/(routes)/subjects/page.tsx
@@ -1,29 +1,32 @@
+import React from "react";
 import SubjectsForm from "@/components/SubjectsForm/subjectsForm";
 import ViewSubjectTable from "@/components/ViewSubjectList/viewSubject";
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
+import { prisma } from "@/lib/prisma";
 
-export default function Subjects() {
+
+export default async function Subjects() {
+  const subjects = await prisma.subject.findMany();
+
+
   return (
-    <div className="flex gap-8 pl-16 flex-col items-center pt-12 w-full h-screen">
-      <h1 className="text-3xl w-full text-left pl-[20rem]">
-        Subjects Page
-      </h1>
+    <div className="flex flex-col items-center pt-12 w-full h-screen">
+      <h1 className="text-3xl w-full text-left pl-[20rem]">Subjects Page</h1>
       <Separator />
-      <h1 className="text-xl w-full text-left pl-[25rem]">Add a Subject</h1>
-      <div>
+      <h2 className="text-xl w-full text-left pl-[25rem]">Add a Subject</h2>
+      <div className="mt-4">
         <Card className="w-[40vw] rounded-sm">
           <CardHeader>
             <CardTitle>Add Subject</CardTitle>
             <CardDescription>
-              Add a Subject if it does not Exist in the below table.
+              Add a Subject if it does not exist in the below table.
             </CardDescription>
           </CardHeader>
           <CardContent>
@@ -32,9 +35,9 @@ export default function Subjects() {
         </Card>
       </div>
       <Separator />
-      <h1 className="text-xl w-full text-left pl-[25rem]">Subject List</h1>
-      <div>
-        <ViewSubjectTable />
+      <h2 className="text-xl w-full text-left pl-[25rem]">Subject List</h2>
+      <div className="mt-4">
+        <ViewSubjectTable subjects={subjects} />
       </div>
     </div>
   );
--- components/ViewSubjectList/viewSubject.tsx
+++ components/ViewSubjectList/viewSubject.tsx
@@ -4,13 +4,10 @@ import React, { useState, useEffect } from "react";
 import { SubjectType, columns } from "./columns";
 import { DataTable } from "./data-table";
 
-import Subjects from "@/app/(client)/(viewquestions)/_components/sample.subjectDB.json";
-
-const ViewSubjectTable = () => {
-
+const ViewSubjectTable = ({ subjects }: { subjects: SubjectType[] }) => {
   return (
     <div className="w-[40vw]">
-      <DataTable columns={columns} data={Subjects} />
+      <DataTable columns={columns} data={subjects} />
     </div>
   );
 };
