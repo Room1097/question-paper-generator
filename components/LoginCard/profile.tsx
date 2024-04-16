0b992e32dfaed18445d3fba6cef89362bd8ab896
salshevde <salonishevdeofficial@gmail.com>
2024-04-16 21:52:57 +0530

    bug fix 7

--- components/LoginCard/profile.tsx
+++ components/LoginCard/profile.tsx
@@ -4,12 +4,13 @@ import {useSession } from "next-auth/react"
 import {Avatar} from "@nextui-org/avatar";
 import { redirect } from "next/navigation";
 import { currProfile } from "@/lib/current-profile";
-const Profile = ()=>{
+const  Profile = ()=>{
     const{data:session } = useSession();
     
     if(session){
 
-    
+        const profile = currProfile(session);
+
         const avatarUrl = session?.user?.image as string;
         return(
             <div className="flex flex-col align-center">
--- lib/current-profile.ts
+++ lib/current-profile.ts
@@ -6,7 +6,7 @@ export async function currProfile(session:Session){
 
    const user = session?.user;
     
-
+    console.log(session)
    try{
     const Profile = await prisma.profile.findUniqueOrThrow({
         where:{
@@ -17,6 +17,7 @@ export async function currProfile(session:Session){
     return Profile
    }
    catch{
+    console.log("New Profile being generated...")
     const newProfile = await prisma.profile.create({
         data: { 
             userId  : crypto.randomUUID() ,
