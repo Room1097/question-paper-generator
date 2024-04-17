5d277ca0debc92a09fc7ce3f7b82b4447e91aae0
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2024-04-17 20:13:18 +0530

    completed apply verification form

--- components/VerificationCard/VerificationForm.tsx
+++ components/VerificationCard/VerificationForm.tsx
@@ -1,38 +1,90 @@
-'use client'
+"use client";
 import React from "react";
-import { Label } from "../ui/label";
-import { Input } from "../ui/input";
-import { useEdgeStore } from "@/lib/edgestore";
-const edgestore = useEdgeStore(); 
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+  name: z.string().min(2).max(50),
+  email: z.string().email().min(1),
+});
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+import { currProfile } from "@/lib/current-profile";
+
 const VerificationForm = () => {
+  const { data: session } = useSession();
+
+  if (session) {
+    const profile = currProfile(session);
+  }
+  const name = session?.user?.name as string;
+  const email = session?.user?.email as string;
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: name || "", 
+      email: email || "", 
+    },
+  });
+
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+
+    
+  }
+
   return (
-    <div className="">
-      <form action="" className="flex flex-col gap-8">
-        <div className="space-y-2">
-          <Label htmlFor="name">Registered Name</Label>
-          <Input type="name" id="name" name="name" required />
-        </div>
-        <div className="space-y-2">
-          <Label htmlFor="email">Registered Email ID</Label>
-          <Input type="email" id="email" name="email" required />
-        </div>
-        <div className="space-y-2">
-          <Label htmlFor="file" className="flex flex-col gap-4">
-            Educational Certificate
-            <span className="text-sm dark:text-zinc-400">
-              Note: UG Students can Apply as well with their HSC/SSC or
-              Equivalent Marksheets. PG Students need to Provide their Bachelors
-              Degree or Diploma or Equivalent Certificate.
-            </span>
-          </Label>
-          <Input type="file" id="email" name="email" required />
-        </div>
-        <div className="dark:text-zinc-400">
-          Note: All your documents will be private and would not be accessible
-          by anyone else.
-        </div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Registered Name</FormLabel>
+              <FormControl>
+                <Input value={name} readOnly />
+              </FormControl>
+              <FormDescription>
+                This is your Registered Name, displayed in the Sidebar.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input defaultValue={email} type="email" />
+              </FormControl>
+              <FormDescription>
+                This is Your Registered Email Id.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Apply</Button>
       </form>
-    </div>
+    </Form>
   );
 };
 
